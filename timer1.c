fb34b7c9c3c9ee03e97b59edc60181394204e91b
omar shaaban <102553384+omar-shaaban742001@users.noreply.github.com>
2023-05-15 04:35:53 +0300

    Add files via upload

---
+++ timer1.c
@@ -0,0 +1,45 @@
+/******************************************************************************
+ * Module:Timer
+ *
+ * File Name: Timer.c
+ *
+ * Description: Source file for Timer
+ *
+ * Author: omar shaaban rabea 
+ ******************************************************************************/
+ 
+#include "..\\./MAIN/tm4c123gh6pm.h" 
+#include "..\\./MAIN/Timer.h"
+
+/*******************************************************************************
+ *                       Variables                             *
+ *******************************************************************************/
+ 
+unsigned long TimerCountt;
+unsigned long Semaphoree;
+
+/*******************************************************************************
+ *                          Functions                             *
+ *******************************************************************************/
+void Timer2_Init(unsigned long period){ 
+  unsigned long volatile delay;
+  SYSCTL_RCGCTIMER_R |= 0x04;   // 0) activate timer2
+  delay = SYSCTL_RCGCTIMER_R;
+  TimerCount = 0;
+  Semaphore = 0;
+  TIMER2_CTL_R = 0x00000000;    // 1) disable timer2A during setup
+  TIMER2_CFG_R = 0x00000000;    // 2) configure for 32-bit mode
+  TIMER2_TAMR_R = 0x00000002;   // 3) configure for periodic mode, default down-count settings
+  TIMER2_TAILR_R = period-1;    // 4) reload value
+  TIMER2_TAPR_R = 0;            // 5) bus clock resolution
+  TIMER2_ICR_R = 0x00000001;    // 6) clear timer2A timeout flag
+  TIMER2_IMR_R = 0x00000001;    // 7) arm timeout interrupt
+  NVIC_PRI5_R = (NVIC_PRI5_R&0x00FFFFFF)|0x80000000; // 8) priority 4
+// interrupts enabled in the main program after all devices initialized
+// vector number 39, interrupt number 23
+  NVIC_EN0_R = 1<<23;           // 9) enable IRQ 23 in NVIC
+  TIMER2_CTL_R = 0x00000001;    // 10) enable timer2A
+	
+	 for(i = 0; i < period; i++) { while ((TIMER2_RIS_R & 0x1) == 0) ;      /* wait for TimerA timeout flag */
+        TIMER2_ICR_R = 0x1;      /* clear the TimerA timeout flag */
+}
