abbfda616a46b04a1c487d13c2ab23711f6d3c6e
omar-mostafa-taha <66518667+omar-mostafa-taha@users.noreply.github.com>
2023-05-16 21:55:14 +0300

    Add files via upload

---
+++ X-O Game Project/drivers/NVIC/NVIC_interface.h
@@ -0,0 +1,14 @@
+#ifndef NVIC_INTERFACE_H_
+
+#define NVIC_INTERFACE_H_
+#include "../../common/Types.h"
+
+
+void NVIC_vidEnableInterrupt(u8 u8InterruptNumber);
+void NVIC_vidDisableInterrupt(u8 u8InterruptNumber);
+void NVIC_vidSetPendingFlag(u8 u8InterruptNumber);
+void NVIC_vidClearPendingFlag(u8 u8InterruptNumber);
+
+u8 NVIC_u8GetActiveFlag(u8 u8InterruptNumber);
+
+#endif
