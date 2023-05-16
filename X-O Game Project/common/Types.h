5adfb6f45b60076ea6a2c392ac689eff104daff4
‪Ahmed Nashaat‬‏ <65959637+ahmednashaat30900@users.noreply.github.com>
2023-05-16 20:52:26 +0300

    Add files via upload

---
+++ X-O Game Project/common/Types.h
@@ -0,0 +1,21 @@
+#ifndef _COMMON_TYPES_H_
+#define _COMMON_TYPES_H_
+
+#define TRUE    1U;
+#define FALSE   0U;
+
+typedef unsigned char   u8;
+typedef signed char     s8;
+
+typedef float           f16;
+
+typedef double          f64;
+
+typedef unsigned short  u16;
+typedef signed short    s16;
+
+typedef unsigned long   u32;
+typedef signed long     s32;
+
+
+#endif
