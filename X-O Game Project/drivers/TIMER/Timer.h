e157ac99598954ff0ee668ac3a061b74024eb6d8
omar shaaban <102553384+omar-shaaban742001@users.noreply.github.com>
2023-05-18 19:10:48 +0300

    Add files via upload

---
+++ X-O Game Project/drivers/TIMER/Timer.h
@@ -0,0 +1,19 @@
+/******************************************************************************
+ * Module:Timer
+ *
+ * File Name: Timer.h
+ *
+ * Description: Header file for Timer
+ *
+ * Authors: omar shaaban rabea - omar mostafa - ahmed nashaat - kareem - mohamed
+ ******************************************************************************/
+
+
+/*******************************************************************************
+ *                      Function Prototypes                                    *
+ *******************************************************************************/
+	
+void Timer2_delay(unsigned long period);
+
+
+
